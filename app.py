a66dda93fe8317362e1b1afa610f85f8ad755c17
allebd <delebella@yahoo.com>
2019-11-04 18:50:28 +0100

    add read items

--- app.py
+++ app.py
@@ -1,14 +1,29 @@
+import os
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+
+# load dotenv in the base root
+APP_ROOT = os.path.join(os.path.dirname(__file__), '.')   # refers to application_top
+dotenv_path = os.path.join(APP_ROOT, '.env')
+load_dotenv(dotenv_path)
+
+DATABASE_URL = os.getenv('DATABASE_URL')
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+  __tablename__ = 'todos'
+  id = db.Column(db.Integer, primary_key=True)
+  description = db.Column(db.String(), nullable=False)
+
+  def __repr__(self):
+    return f'<Todo {self.id} {self.description}>'
+
+db.create_all()
 
 @app.route('/')
 def index():
-  return render_template('index.html', data=[{
-    'description': 'Todo 1'
-  }, {
-    'description': 'Todo 2'
-  }, {
-    'description': 'Todo 3'
-  }])
-  
+  return render_template('index.html', data=Todo.query.all())
