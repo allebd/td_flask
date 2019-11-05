755b54601d652534dd5c80db87d57f6e5784f887
allebd <delebella@yahoo.com>
2019-11-05 13:52:52 +0100

    add sessions

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-import os
-from flask import Flask, render_template, request, jsonify
+import os, sys
+from flask import Flask, render_template, request, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
 
@@ -26,13 +26,24 @@ db.create_all()
 
 @app.route('/todos/create', methods=['POST'])
 def create_todo():
-  description = request.get_json()['description']
-  todo =  Todo(description=description)
-  db.session.add(todo)
-  db.session.commit()
-  return jsonify({
-    'description': todo.description
-  })
+  error = False
+  body = {}
+  try:
+    description = request.get_json()['description']
+    todo =  Todo(description=description)
+    db.session.add(todo)
+    db.session.commit()
+    body['description'] = todo.description
+  except:
+    error = True
+    db.session.rollback()
+    print(sys.exc_info())
+  finally:
+    db.session.close()
+    if error:
+      abort (400)
+    else:
+      return jsonify(body)
 
 @app.route('/')
 def index():
